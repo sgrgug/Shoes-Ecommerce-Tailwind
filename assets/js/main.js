bd6a17cf6b4d4ec07859aa3d48fd4f26c2127c8b
Sagar Gurung <sgrgug@gmail.com>
2023-05-27 12:48:13 +0545

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 window.addEventListener('scroll', function () {
     var nav_top = document.getElementById('nav_top');
     var getScrollValue = window.scrollY;
-  
+
     if (getScrollValue > 100) {
         nav_top.classList.add('bg-pri-100');
         nav_top.classList.add('shadow-xl');
@@ -10,5 +10,17 @@ window.addEventListener('scroll', function () {
         nav_top.classList.remove('bg-pri-100');
         nav_top.classList.remove('shadow-xl');
     }
-  });
-  
+});
+
+//for menu
+var menu = document.getElementById('menu');
+var menu_btn = document.getElementById('menu_btn');
+menu_btn.addEventListener('click', function(){
+    if(menu.classList.contains('hidden')){
+        menu.classList.remove('hidden');
+        menu.classList.add('block');
+    }else{
+        menu.classList.add('hidden');
+        menu.classList.remove('block');
+    }
+});
