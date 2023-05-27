17e504a314852d05aca671a5bd8ea053b3f88909
Sagar Gurung <sgrgug@gmail.com>
2023-05-27 11:37:32 +0545

    header added

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', function () {
+    var nav_top = document.getElementById('nav_top');
+    var getScrollValue = window.scrollY;
+  
+    if (getScrollValue > 100) {
+        nav_top.classList.add('bg-pri-100');
+        nav_top.classList.add('shadow-xl');
+    } else {
+        nav_top.classList.remove('bg-pri-100');
+        nav_top.classList.remove('shadow-xl');
+    }
+  });
+  
